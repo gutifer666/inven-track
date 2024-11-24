429f8520f7efdbe59c76eb5f32fa36a5a70b9509
Javier <gutifer666@gmail.com>
2024-11-24 23:48:42 +0100

    Init commit

---
+++ src/main/java/com/javiergutierrez/inven_track/InvenTrackApplication.java
@@ -0,0 +1,13 @@
+package com.javiergutierrez.inven_track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvenTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvenTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javiergutierrez/inven_track/InvenTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javiergutierrez.inven_track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvenTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
