a2a0c9b8e24ebe5840263b78b72957ed56b28328
Javier <gutifer666@gmail.com>
2024-12-04 01:09:45 +0100

    DataUserDetails

--- src/main/java/com/javiergutierrez/inven_track/modules/users/infrastructure/repositories/IJpaUserRepository.java
+++ src/main/java/com/javiergutierrez/inven_track/modules/users/infrastructure/repositories/IJpaUserRepository.java
@@ -2,13 +2,12 @@ package com.javiergutierrez.inven_track.modules.users.infrastructure.repositorie
 
 import com.javiergutierrez.inven_track.modules.users.infrastructure.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface IJpaUserRepository extends JpaRepository<UserEntity, Long> {
 
+	Optional<UserEntity> findByUsername(String username);
 }
--- src/main/java/com/javiergutierrez/inven_track/security/configuration/SecurityConfig.java
+++ src/main/java/com/javiergutierrez/inven_track/security/configuration/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -14,6 +16,9 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http
 				.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(authorize -> authorize
+						.requestMatchers(new AntPathRequestMatcher("/users", "POST")).permitAll()
+						.anyRequest().authenticated())
 				.build();
 	}
 }
